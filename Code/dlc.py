686351c222d8ffb4c5e9ce708b414bed58405de7
neuromantic99 <jrowlandm@gmail.com>
2024-08-19 15:14:11 +0100

    switch to cropped video

--- Code/dlc.py
+++ Code/dlc.py
@@ -3,10 +3,10 @@ from pathlib import Path
 
 HERE = Path(__file__).parent
 path_to_pupil_to_hyponogram_data = Path("/home/james/Code/Pupil-to-Hypnogram/Pupil-to-hypnogram-data")
-path_video = HERE.parent / "data" / "eyeCamleft.mj2"
+path_video = HERE.parent / "data" / "croppedPupil.mp4"
 
 path_config_file = path_to_pupil_to_hyponogram_data /  "Intermediate/DLC/DLC-Kobayashi-2021-07-19/config.yaml"
 
 deeplabcut.analyze_videos(
-    path_config_file, [str(path_video)], videotype="mj2", save_as_csv=True
+    path_config_file, [str(path_video)], videotype="mp4", save_as_csv=True
 )
